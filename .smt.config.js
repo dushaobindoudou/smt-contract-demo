611ea09b3a0e851bec6c54da0332825084d293d2
dushaobin <dushaobindoudou@gmail.com>
2019-01-12 17:32:30 +0800

    初始化项目

---
+++ .smt.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "projectType": "react",
+    "prefix": "",
+    "publicPath": "/",
+    "dist": "../../dist",
+    "clientAlias": "client",
+    "isInlineCss": "false",
+    "smtConfig": "./.smtConfig/",
+    "smtWebpackConfigDir": "{{smtConfig}}/webpack/",
+    "smtManifsetDir": "{{smtConfig}}/manifest/",
+    "configIn": "client",
+    contract: {
+        network: {
+            url: '', // infure 地址
+            mnemonic: '',
+        },
+        ext: '.sol', // 编译文件的扩展名
+        sourceDir: 'contract/source/',
+        buildDist: 'contract/dist/',
+        buildConfig: {
+            language: 'Solidity',
+            settings: {
+                outputSelection: {
+                    '': {
+                        '*': '*'
+                    },
+                    '*': {
+                        '*': [ '*' ]
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ contract/source/Migrations.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+import "./zilb/MetaCoin1.sol";
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    uint am = ConvertLib.convert(20, 20);
+    last_completed_migration = completed + am;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
+}
